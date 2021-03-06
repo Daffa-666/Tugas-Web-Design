c73410e6f1d883b4864ed385b672df766e0779b5
Sebut saja Daffa <79488250+Daffa-666@users.noreply.github.com>
2021-03-07 03:25:54 +0800

    Add files via upload

---
+++ WEBDESIGN/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FXTRT.com</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container" id="navbar">
+      <div class="navbar-header">
+        <a href="#" class="navbar-brand navbar-link"><img src="assets/img/logo.png"></a>
+        <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      </div>
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li role="presentation"><a href="#home">HOME</a></li>
+          <li role="presentation"><a href="#news">NEWS</a></li>
+          <li role="presentation"><a href="#gallery">GALLERY</a></li>
+          <li role="presentation"><a href="#about">ABOUT</a></li>
+          <li ><a href="assets/form/form.html">SIGN IN</a></li>
+          <li role="presentation"><a href="#"><i class="glyphicon glyphicon-search"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- navbar -->
+
+  <!-- jumbotron -->
+  <div id="background">
+    <div class="jumbotron">
+      <p>Menjual Berbagai Macam Preset Lightroom</p>
+    </div>
+  </div>
+  <!-- jumbotron -->
+
+  <!-- container atas -->
+  <div id="home">
+    <div class="isi">
+      <div class="container atas">
+        <h1>Best Seller Preset</h1>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i class="glyphicon glyphicon-map-marker"></i>
+            <h3>Dark Moody</h3>
+            <p class="text-justify"><strong>DarkMoody</strong>Preset yang mempunyai karakter Dark (gelap) dan menambahkan efek fokus ke tengah dan Preset ini digunakan di lokasi tertentu.</p>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i class="glyphicon glyphicon-tree-conifer"></i>
+            <h3>Dark Nature</h3>
+            <p class="text-justify"><strong>DarkNature</strong>Preset yang mempunyai karakter Dark (gelap) namun Preset ini di peruntukkan untuk pepohonan atau alam.</p>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <i class="glyphicon glyphicon-camera"></i>
+            <h3>Exotic Bright</h3>
+            <p class="text-justify"><strong>ExoticBright</strong>Preset yang mempunyai karakter kecerahan yang exotic namun Preset ini di peruntukkan untuk tipe lensa camera yang intensitas cahaya nya sedikit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- container atas -->
+
+  <!-- container bawah -->
+  <div class="container bawah">
+    <h1></h1>
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img class="img-circle" src="assets/img/gambar-1-A.jpg" width="200px">
+        <h3>Dark Moody</h3>
+        <p class="text-center"><strong></strong></p>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img class="img-circle" src="assets/img/gambar-1-B.jpg" width="200px">
+        <h3>Dark Nature</h3>
+        <p class="text-center"><strong></strong></p>
+      </div><div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img class="img-circle" src="assets/img/gambar-1-C.jpg" width="200px">
+        <h3>Exotic Bright</h3>
+        <p class="text-center"><strong></strong></p>
+      </div>
+    </div>
+  </div>
+  <!-- container bawah -->
+
+  <!-- container news -->
+  <div id="news">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/gambar-2-A.jpg">
+           <div class="caption">
+            <h3>Gambar 1</h3>
+            <p class="text-justify">Foto ini diambil menggunakan kamera Smartphone berjenis Android sekitar pukul 06:25 WITA di desa Pinggan Kintamani,Bali. Dan foto ini menggunakan Preset Exotic Bright</p>
+            <button class="btn btn-default"tyoe="button">READ MORE</button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/gambar-2-B.jpg">
+          <div class="caption">
+            <h3>Gambar 2</h3>
+            <p class="text-justify">Foto ini diambil menggunakan kamera Smartphone berjenis Android sekitar pukul 14:45 WITA di halaman rumah saya dan foto ini menggunakan preset Dark Moody</p>
+            <button class="btn btn-default"tyoe="button">READ MORE</button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/gambar-2-C.jpg">
+          <div class="caption">
+            <h3>Gambar 3</h3>
+            <p class="text-justify">Foto ini diambil menggunakan kamera Smartphone berjenis Android sekitar pukul 15:34 WITA di halaman rumah saya Dan foto ini menggunakan Preset Dark Moody</p>
+            <button class="btn btn-default"tyoe="button">READ MORE</button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+          <div class="thumbnail"><img src="assets/img/gambar-2-D.jpg">
+          <div class="caption">
+            <h3>Gambar 4</h3>
+            <p class="text-justify">Foto ini diambil menggunakan kamera Smartphone berjenis Android sekitar pukul 15:45 WITA di halaman rumah saya Dan foto ini menggunakan Preset Dark Nature</p>
+            <button class="btn btn-default"tyoe="button">READ MORE</button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- container news -->
+
+  <!-- gallery -->
+  <div id="gallery">
+    <div class="container">
+      <h1>Gallery</h1>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="satu"><img class="img-responsive" src="assets/img/gambar-3-A.jpg" width="3000px"></div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><img class="img-responsive" src="assets/img/gambar-3-B.jpg" width="300px"></div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="satu"><img class="img-responsive" src="assets/img/gambar-3-C.jpg" width="300px"></div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="dua"><img class="img-responsive" src="assets/img/gambar-3-D.jpg" width="600px"></div>
+      </div>
+    </div>
+  </div>
+  <!-- gallery -->
+
+  <!-- about -->
+  <div id="about">
+    <div class="container footer">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+      <img src="./assets/img/logoo.png" width="180px">
+        <h2>About Us</h2>
+        <p> @_fxtrt                                                                                                            +62 81339 4587 69</p>
+      </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <h2>Newsletter Subscription</h2>
+          <div class="input-group input-group-lg">
+            <input type="text" class="form-control" value="Your Email">
+            <div class="input-group-btn">
+              <button class="btn btn-primary" type="submit">Subscribe </button>
+            </div>
+          </div>
+          <div id="icon"><i class="fa fa-instagram"></i><i class="fa fa-facebook-official"></i><i class="fa fa-twitter-square"></i><i class="fa fa-youtube-play"></i></div>
+        </div>
+    </div>
+  </div>
+  <!-- about -->
+
+  <!-- kaki -->
+  <div id="kaki">
+    <div class="container">
+      <h5 class="text-center">FXTRT © 2021</h5>
+    </div>
+  </div>
+  <!-- kaki -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
